3df9c3e08556e696e425f12a5dcb5ebb9e5af8e2
Sentienn <fdhl.ahmd06@gmail.com>
2026-04-22 17:16:45 +0700

    tambah class game dan gameStatus

---
+++ TubesKPL_Kelompok_2/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Game
+{
+    public int Id {  get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public GameStatus Status { get; set; }
+
+    public Game(int id, string name, int price)
+    {
+        if (id <= 0)
+            throw new Exception("Id Harus lebih dari 0");
+        if (string.IsNullOrEmpty(name))
+            throw new Exception("Nama game tidak boleh kosong");
+        if (price < 0)
+            throw new Exception("Harga tidak boleh negatif");
+
+        Id = id;
+        Name = name;
+        Price = price;
+        Status = GameStatus.NotOwned;
+
+        if (Status != GameStatus.NotOwned)
+            throw new Exception("Status awal harus NotOwned");
+    }
+
+    public Game() { }
+
+}
