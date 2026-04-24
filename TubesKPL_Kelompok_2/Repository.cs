1742ec4edcaf9efb034ff879cb2fcc0d548279e6
Zikry <ilovegreen875@gmail.com>
2026-04-25 01:28:29 +0700

    Nambah repo dan games json

---
+++ TubesKPL_Kelompok_2/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+class Repository<T>
+{
+    public List<T> Load(string file)
+    {
+        if (!File.Exists(file))
+            throw new Exception("File tidak ditemukan");
+
+        string json = File.ReadAllText(file);
+
+        var data = JsonSerializer.Deserialize<List<T>>(json);
+
+        if (data == null)
+            throw new Exception("Gagal membaca data");
+
+        return data;
+    }
+}
