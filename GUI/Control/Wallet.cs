56a9919ce04d9f756e3e05e0246303b86ffa7024
DafferNx <filbertdeven20@gmail.com>
2026-06-13 09:03:38 +0700

    Tambah Projek GUI ke Solution

---
+++ GUI/Control/Wallet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class UserControl4 : UserControl
+    {
+        public UserControl4()
+        {
+            InitializeComponent();
+        }
+    }
+}
