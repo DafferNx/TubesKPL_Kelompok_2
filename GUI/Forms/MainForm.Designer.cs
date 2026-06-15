14ae4f2dbe532f7f1181a3e1cbc6cdf84ab0d919
Rangga Luthfiendi <echo.rangrang@gmail.com>
2026-06-15 15:38:33 +0700

    Perbaiki Design Main Form

--- GUI/Forms/MainForm.Designer.cs
+++ GUI/Forms/MainForm.Designer.cs
@@ -2,30 +2,15 @@
 {
     partial class MainForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             NavPanel = new Panel();
@@ -40,84 +25,113 @@
             // 
             // NavPanel
             // 
-            NavPanel.Controls.Add(btnLogout);
-            NavPanel.Controls.Add(btnCart);
-            NavPanel.Controls.Add(btnWallet);
-            NavPanel.Controls.Add(btnLibrary);
+            NavPanel.BackColor = Color.FromArgb(28, 28, 28);
             NavPanel.Controls.Add(btnStore);
+            NavPanel.Controls.Add(btnLibrary);
+            NavPanel.Controls.Add(btnWallet);
+            NavPanel.Controls.Add(btnCart);
+            NavPanel.Controls.Add(btnLogout);
             NavPanel.Dock = DockStyle.Left;
             NavPanel.Location = new Point(0, 0);
             NavPanel.Name = "NavPanel";
-            NavPanel.Size = new Size(163, 450);
+            NavPanel.Size = new Size(180, 520);
             NavPanel.TabIndex = 0;
             // 
             // PagePanel
             // 
+            PagePanel.BackColor = Color.FromArgb(28, 28, 28);
             PagePanel.Dock = DockStyle.Fill;
-            PagePanel.Location = new Point(163, 0);
+            PagePanel.Location = new Point(180, 0);
             PagePanel.Name = "PagePanel";
-            PagePanel.Size = new Size(637, 450);
+            PagePanel.Size = new Size(620, 520);
             PagePanel.TabIndex = 1;
             // 
             // btnStore
             // 
-            btnStore.Location = new Point(24, 22);
+            btnStore.BackColor = Color.FromArgb(45, 45, 45);
+            btnStore.FlatAppearance.BorderSize = 0;
+            btnStore.FlatStyle = FlatStyle.Flat;
+            btnStore.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            btnStore.ForeColor = Color.White;
+            btnStore.Location = new Point(20, 25);
             btnStore.Name = "btnStore";
-            btnStore.Size = new Size(112, 34);
+            btnStore.Size = new Size(140, 45);
             btnStore.TabIndex = 0;
             btnStore.Text = "Store";
-            btnStore.UseVisualStyleBackColor = true;
+            btnStore.UseVisualStyleBackColor = false;
             // 
             // btnLibrary
             // 
-            btnLibrary.Location = new Point(24, 78);
+            btnLibrary.BackColor = Color.FromArgb(45, 45, 45);
+            btnLibrary.FlatAppearance.BorderSize = 0;
+            btnLibrary.FlatStyle = FlatStyle.Flat;
+            btnLibrary.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            btnLibrary.ForeColor = Color.White;
+            btnLibrary.Location = new Point(20, 85);
             btnLibrary.Name = "btnLibrary";
-            btnLibrary.Size = new Size(112, 34);
+            btnLibrary.Size = new Size(140, 45);
             btnLibrary.TabIndex = 1;
             btnLibrary.Text = "Library";
-            btnLibrary.UseVisualStyleBackColor = true;
+            btnLibrary.UseVisualStyleBackColor = false;
             // 
             // btnWallet
             // 
-            btnWallet.Location = new Point(24, 141);
+            btnWallet.BackColor = Color.FromArgb(45, 45, 45);
+            btnWallet.FlatAppearance.BorderSize = 0;
+            btnWallet.FlatStyle = FlatStyle.Flat;
+            btnWallet.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            btnWallet.ForeColor = Color.White;
+            btnWallet.Location = new Point(20, 145);
             btnWallet.Name = "btnWallet";
-            btnWallet.Size = new Size(112, 34);
+            btnWallet.Size = new Size(140, 45);
             btnWallet.TabIndex = 2;
             btnWallet.Text = "Wallet";
-            btnWallet.UseVisualStyleBackColor = true;
+            btnWallet.UseVisualStyleBackColor = false;
             // 
             // btnCart
             // 
-            btnCart.Location = new Point(24, 203);
+            btnCart.BackColor = Color.FromArgb(45, 45, 45);
+            btnCart.FlatAppearance.BorderSize = 0;
+            btnCart.FlatStyle = FlatStyle.Flat;
+            btnCart.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            btnCart.ForeColor = Color.White;
+            btnCart.Location = new Point(20, 205);
             btnCart.Name = "btnCart";
-            btnCart.Size = new Size(112, 34);
+            btnCart.Size = new Size(140, 45);
             btnCart.TabIndex = 3;
             btnCart.Text = "Cart";
-            btnCart.UseVisualStyleBackColor = true;
+            btnCart.UseVisualStyleBackColor = false;
             // 
             // btnLogout
             // 
-            btnLogout.Location = new Point(24, 360);
+            btnLogout.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnLogout.BackColor = Color.FromArgb(60, 60, 60);
+            btnLogout.FlatAppearance.BorderSize = 0;
+            btnLogout.FlatStyle = FlatStyle.Flat;
+            btnLogout.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            btnLogout.ForeColor = Color.FromArgb(200, 200, 200);
+            btnLogout.Location = new Point(20, 450);
             btnLogout.Name = "btnLogout";
-            btnLogout.Size = new Size(112, 34);
+            btnLogout.Size = new Size(140, 45);
             btnLogout.TabIndex = 4;
             btnLogout.Text = "Logout";
-            btnLogout.UseVisualStyleBackColor = true;
+            btnLogout.UseVisualStyleBackColor = false;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(28, 28, 28);
+            ClientSize = new Size(800, 520);
             Controls.Add(PagePanel);
             Controls.Add(NavPanel);
+            MinimumSize = new Size(640, 400);
             Name = "MainForm";
-            Text = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SETIM";
             NavPanel.ResumeLayout(false);
             ResumeLayout(false);
         }
-
-        #endregion
 
         private Panel NavPanel;
         private Button btnLogout;
